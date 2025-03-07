2703154224ac74288e8b6ce0e6e97e8fc1c3162f
1602saurab <saurabhsonibrd@gmail.com>
2025-03-07 11:17:02 +0530

    updated_yaml

--- src/train.py
+++ src/train.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np
+#import numpy as np
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
